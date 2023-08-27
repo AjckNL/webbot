4e2989d47f01500db44903eedea78c3a86b31b47
Niguious <13286978@student.uts.edu.au>
2023-08-27 20:37:21 +1000

    Front-end Webbot Management

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Needs to import Figma UI on here
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://marketplace.visualstudio.com/items?itemName=figma.figma-vscode-extension"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          with VS code
         </a>
       </header>
     </div>
---
+++ src/Page1.js
@@ -0,0 +1,114 @@
+import React from "react";
+import "./Page1.css";
+
+function Desktop () {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="text-wrapper">Bot Management</div>
+        <div className="navigation-bar">
+          <div className="database">
+            <div className="text-wrapper-2">Database</div>
+            <img className="database-img" alt="Database img" src="database-img.png" />
+          </div>
+          <div className="text-wrapper-3">HomePage</div>
+          <div className="insight">
+            <div className="text-wrapper-4">Insight</div>
+            <img className="insight-img" alt="Insight img" src="insight-img.png" />
+          </div>
+          <div className="search">
+            <div className="overlap-group">
+              <div className="text-wrapper-5">Search</div>
+              <img className="search-img" alt="Search img" src="search-img.png" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap">
+          <div className="bot-management">
+            <div className="overlap-2">
+              <div className="actions">
+                <div className="ellipse" />
+                <div className="ellipse-2" />
+                <div className="ellipse-3" />
+              </div>
+              <div className="group">
+                <div className="data">
+                  <div className="text-wrapper-6">Busy</div>
+                  <div className="ellipse-4" />
+                  <div className="text-wrapper-7">Target; Amazon ...</div>
+                  <div className="text-wrapper-8">3 Days</div>
+                  <div className="text-wrapper-9">3</div>
+                  <div className="text-wrapper-10">iPhone 14 Pro Max</div>
+                  <div className="text-wrapper-11">Mozilla/5.0 (X11; Linux x86_64)...</div>
+                </div>
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group-2">
+                    <img className="line" alt="Line" src="line-8.svg" />
+                    <div className="text-wrapper-12">Delete</div>
+                    <div className="text-wrapper-13">Stop</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="navbar">
+              <div className="text-wrapper-14">id</div>
+              <div className="text-wrapper-15">websites</div>
+              <div className="text-wrapper-16">status</div>
+              <div className="text-wrapper-17">user-agent</div>
+              <div className="text-wrapper-18">Schedule</div>
+              <div className="text-wrapper-19">Product Name</div>
+              <div className="text-wrapper-20">Actions</div>
+            </div>
+            <div className="data-2">
+              <div className="text-wrapper-21">Stop</div>
+              <div className="ellipse-5" />
+              <div className="actions-2">
+                <div className="ellipse" />
+                <div className="ellipse-2" />
+                <div className="ellipse-3" />
+              </div>
+              <div className="text-wrapper-7">Target; Amazon ...</div>
+              <div className="text-wrapper-8">10 Days</div>
+              <div className="text-wrapper-9">1</div>
+              <div className="text-wrapper-10">iPhone 14 Pro Max</div>
+              <div className="text-wrapper-11">Mozilla/5.0 (X11; Linux x86_64)...</div>
+            </div>
+            <img className="image" alt="Image" src="image-2.png" />
+            <div className="overlap-wrapper">
+              <div className="overlap-3">
+                <div className="data-3">
+                  <div className="text-wrapper-6">OK</div>
+                  <div className="ellipse-6" />
+                  <div className="text-wrapper-7">Target; Amazon ...</div>
+                  <div className="text-wrapper-8">3 Days</div>
+                  <div className="text-wrapper-9">2</div>
+                  <div className="text-wrapper-10">iPhone 14 Pro Max</div>
+                  <div className="text-wrapper-11">Mozilla/5.0 (X11; Linux x86_64)...</div>
+                </div>
+                <img className="img" alt="Image" src="image-3.png" />
+              </div>
+            </div>
+          </div>
+          <div className="group-2">
+            <img className="line-2" alt="Line" src="line-6.svg" />
+            <img className="line-3" alt="Line" src="line-5.svg" />
+            <img className="line-4" alt="Line" src="line-1.svg" />
+            <img className="line-5" alt="Line" src="line-2.svg" />
+            <img className="line-6" alt="Line" src="line-7.svg" />
+            <img className="line-7" alt="Line" src="line-4.svg" />
+            <img className="line-8" alt="Line" src="line-3.svg" />
+          </div>
+          <div className="div-wrapper">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src="image.svg" />
+              <div className="text-wrapper-12">Delete</div>
+              <div className="text-wrapper-13">Start</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop;
